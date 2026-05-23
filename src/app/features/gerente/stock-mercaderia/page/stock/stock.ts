6a5150c7fb37439151d96d0141b3679d17b32a24
Urielito1031 <urielestebanyurquina@gmail.com>
2026-05-23 01:41:03 -0300

    agregando mejora en ui/boton para que pueda ser reutilizable

--- src/app/features/gerente/stock-mercaderia/page/stock/stock.ts
+++ src/app/features/gerente/stock-mercaderia/page/stock/stock.ts
@@ -3,10 +3,11 @@ import { StockList } from '../../components/stock-list/stock-list';
 import { StockToolbar } from '../../components/stock-toolbar/stock-toolbar';
 import { CommonModule } from '@angular/common';
 import { ProductoStockMock, PRODUCTOS_STOCK_MOCK } from '../../../../../core/model/producto-stock-mock';
+import { Boton } from "../../../../../shared/ui/boton/boton";
 
 @Component({
   selector: 'app-stock',
-  imports: [StockList,StockToolbar,CommonModule],
+  imports: [StockList, StockToolbar, CommonModule, Boton],
   templateUrl: './stock.html',
   styleUrl: './stock.css',
 })
@@ -14,6 +15,7 @@ export class Stock {
   productosMock = signal<ProductoStockMock[]>(PRODUCTOS_STOCK_MOCK);
   termino = signal<string>('');
   categoria = signal<string>('');
+  tabSeleccionada = signal<'productos' | 'bodegas' | 'lotes'>('productos');
 
   productosFiltrados = computed(() => {
     let lista = this.productosMock();
---
+++ src/app/shared/ui/boton/boton.ts
@@ -0,0 +1,38 @@
+import { CommonModule } from '@angular/common';
+import { Component, computed, input, output } from '@angular/core';
+
+export type BotonVariante = 'primary' | 'secondary' | 'danger' | 'outline' | 'tab';
+export type BotonTamanio = 'sm' | 'md' | 'lg';
+
+@Component({
+  selector: 'app-boton',
+  imports: [CommonModule],
+  templateUrl: './boton.html',
+  styleUrl: './boton.css',
+})
+export class Boton {
+  
+  
+  label = input.required<string>();
+  variante = input<BotonVariante>('primary');
+  color = input<string>();
+  tamanio = input<BotonTamanio>('md');
+  type = input<'button' | 'submit' | 'reset'>('button');
+  activo = input<boolean>(false);
+
+  disabled = input<boolean>(false);
+
+  clicked = output<MouseEvent>();
+
+  
+  clasesCalculadas = computed(() => {
+    const baseClasses = `btn btn-${this.tamanio()} btn-${this.variante()}`;
+    return this.activo() ? `${baseClasses} active` : baseClasses;
+  });
+
+  manejarClick(event: MouseEvent) {
+    if (!this.disabled()) {
+      this.clicked.emit(event);
+    }
+  }
+}
