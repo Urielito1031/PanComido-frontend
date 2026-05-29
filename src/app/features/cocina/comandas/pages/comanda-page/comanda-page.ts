c635c940b0b2858ff6f5ede7a3a2e1397ba11fda
Urielito1031 <urielestebanyurquina@gmail.com>
2026-05-29 13:14:17 -0300

    agregnado componente de comanda page

---
+++ src/app/features/cocina/comandas/pages/comanda-page/comanda-page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comanda-page',
+  imports: [],
+  templateUrl: './comanda-page.html',
+  styleUrl: './comanda-page.css',
+})
+export class ComandaPage {
+  
+
+}
