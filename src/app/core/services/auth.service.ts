12921b040f9eb59af23177879bea890cc70d4aff
Mati <matu.dw@gmail.com>
2026-06-03 14:34:29 -0300

    fix de comandas y ux/ui de cliente al momento de pedir

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -14,8 +14,7 @@ const DEFAULT_ROUTE = 'staff/cocina';
   providedIn: 'root'
 })
 export class AuthService {
-  currentRole = signal<string>('Cocina');
-
+  currentRole = signal<string>('Mozo');
   validateManagerCredentials(username: string, password: string): Observable<boolean> {
     const esValido = username.toLowerCase().trim() === 'gerente' && password === '123456';
     return of(esValido).pipe(delay(250));
