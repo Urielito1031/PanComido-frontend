596a5508c11a01db226572ff351147d603a09f26
Urielito1031 <urielestebanyurquina@gmail.com>
2026-06-05 18:47:52 -0300

    agrego config de coverage en vitest con v8, excludes de interfaces/routes/mocks, thresholds mejorar porcentaje de coverage a minimo 70%

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,6 +8,43 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: ['src/test.setup.ts']
+    setupFiles: ['src/test.setup.ts'],
+    coverage: {
+      provider: 'v8',
+
+      // Qué archivos medir (SOLO código de producción)
+      include: ['src/app/**/*.ts'],
+
+      // Qué EXCLUIR (lo que baja los números artificialmente)
+      exclude: [
+        'src/app/**/*.spec.ts',           // Tests
+        'src/app/**/*.mock.ts',           // Mocks
+        'src/app/**/*.d.ts',
+        'src/app/core/models/**',         // Interfaces (son tipos, no código)
+        'src/app/**/index.ts',
+        'src/app/app.routes.ts',          // Configuración
+        'src/app/app.constants.ts',
+        'src/test.setup.ts',
+        'src/app/**/*/routes.ts',
+        'src/app/core/interceptors/handlers/**'
+      ],
+
+      // Formatos de reporte
+      reporter: ['text', 'html'],
+
+      // HTML para revisar archivo por archivo
+      reportsDirectory: 'coverage',
+
+
+      //Son porcentajes minimos para que el CI pase
+
+      //importante que lo mantengamos en un 70 aprox
+      thresholds: {
+        statements: 35,   // Estás en 36.35%, 
+        branches: 14,     // Estás en 14.46%,
+        functions: 25,    // Estás en 25.69%,
+        lines: 37         // Estás en 37.09%, 
+      }
+    }
   }
 });
