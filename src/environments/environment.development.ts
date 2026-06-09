2fc28efd6dd146d7db55485fb796144e41d67ddb
Carla <101598008+Carla-Stramandinoli@users.noreply.github.com>
2026-06-09 03:07:48 -0300

    empezada la configuracion del local, empezados la eleccion de metodos de pago

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
   //TODO: no borrar durante el desarrollo
-  //apiUrl: 'https://localhost:7204',
-  apiUrl: 'http://localhost:5173',
+  apiUrl: 'https://localhost:7204',
+  //apiUrl: 'http://localhost:5173',
 
   useMock: false
 };
