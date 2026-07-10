b8ba28b4ca4756d3402c3625968665838f5441bc
LuketoDev <lucasbravouni@gmail.com>
2026-07-10 07:03:43 -0300

    boton logout mozo realizado

--- src/app/layouts/mozo-layout/mozo-layout.ts
+++ src/app/layouts/mozo-layout/mozo-layout.ts
@@ -1,13 +1,23 @@
-import { Component , ChangeDetectionStrategy} from '@angular/core';
+import { Component , ChangeDetectionStrategy, inject} from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { MozoTabNavigation } from '../../features/mozo/components/mozo-tab-navigation/mozo-tab-navigation';
+import { AuthService } from '../../core/services/auth.service';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-mozo-layout',
   standalone: true,
-  imports: [RouterModule, MozoTabNavigation],
+  imports: [RouterModule, MozoTabNavigation, FontAwesomeModule],
   templateUrl: './mozo-layout.html',
   styleUrl: './mozo-layout.css'
 })
-export class MozoLayout {}
+export class MozoLayout {
+  private authService = inject(AuthService);
+  readonly faRightFromBracket = faRightFromBracket;
+
+  logout(): void {
+    this.authService.logout();
+  }
+}
