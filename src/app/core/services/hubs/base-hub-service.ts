d59b127e59072410323d6a800ae1ccd97bdbf68c
LuketoDev <lucasbravouni@gmail.com>
2026-07-10 14:41:10 -0300

    cambio en el front para se saltee conexion previa http

--- src/app/core/services/hubs/base-hub-service.ts
+++ src/app/core/services/hubs/base-hub-service.ts
@@ -5,44 +5,46 @@ import { environment } from '../../../../environments/environment';
   providedIn: 'root',
 })
 export class SignalRConexionService {
-  
-  
+
+
   public hub!: signalR.HubConnection;
   private baseUrl = environment.apiUrl;
 
-  private promesaInicio:Promise<void>|null=null;
-  constructor(){
+  private promesaInicio: Promise<void> | null = null;
+  constructor() {
     this.hub = new signalR.HubConnectionBuilder()
-    .withUrl(`${this.baseUrl}/hubs/pancomido`,{
-      withCredentials:true,
-    }).withAutomaticReconnect()
-    .build();
+      .withUrl(`${this.baseUrl}/hubs/pancomido`, {
+        skipNegotiation: true,
+        transport: signalR.HttpTransportType.WebSockets,
+        withCredentials: true
+      }).withAutomaticReconnect()
+      .build();
   }
-  public iniciar():Promise<void> {
-    if(this.hub.state === signalR.HubConnectionState.Connected){
+  public iniciar(): Promise<void> {
+    if (this.hub.state === signalR.HubConnectionState.Connected) {
       return Promise.resolve();
     }
     // Patrón Singleton de Promesa: Si nadie está conectando, iniciamos el proceso
     if (!this.promesaInicio) {
       this.promesaInicio = this.hub.start()
         .then(() => {
-          
+
         })
         .catch(error => {
-          
+
           this.promesaInicio = null; // Reseteamos para que se pueda reintentar
           throw error; // Propagamos el error al servicio de dominio
         });
     }
     return this.promesaInicio;
   }
-  public detener():void{
-    if(this.hub.state === signalR.HubConnectionState.Connected){
+  public detener(): void {
+    if (this.hub.state === signalR.HubConnectionState.Connected) {
       this.hub.stop();
-      this.promesaInicio =null;
-      
+      this.promesaInicio = null;
+
     }
   }
 
- 
+
 }
